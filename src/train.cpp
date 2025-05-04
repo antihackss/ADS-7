d3d9e7d3d02034865c7dc5a21ad2167bb8086c0b
antihackss <vladmakov0859@gmail.com>
2025-05-04 14:51:55 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -4,142 +4,142 @@
 Train::Train() : countOp(0), first(nullptr), current(nullptr) {}
 
 Train::~Train() {
-    if (!first) return;
-    
-    Car* tempCar = first->next;
-    while (tempCar != first) {
-        Car* nextCar = tempCar->next;
-        delete tempCar;
-        tempCar = nextCar;
-    }
-    delete first;
+  if (!first) return;
+
+  Car* temp_car = first->next;
+  while (temp_car != first) {
+    Car* next_car = temp_car->next;
+    delete temp_car;
+    temp_car = next_car;
+  }
+  delete first;
 }
 
 void Train::addCar(bool light) {
-    Car* newCar = new Car{light, nullptr, nullptr};
-    
-    if (!first) {
-        first = newCar;
-        first->next = first;
-        first->prev = first;
-        current = first;
-    } else {
-        Car* lastCar = first->prev;
-        lastCar->next = newCar;
-        newCar->prev = lastCar;
-        newCar->next = first;
-        first->prev = newCar;
-    }
+  Car* new_car = new Car{light, nullptr, nullptr};
+
+  if (!first) {
+    first = new_car;
+    first->next = first;
+    first->prev = first;
+    current = first;
+  } else {
+    Car* last_car = first->prev;
+    last_car->next = new_car;
+    new_car->prev = last_car;
+    new_car->next = first;
+    first->prev = new_car;
+  }
 }
 
 int Train::getLength() {
-    if (!first) return 0;
-    
-    countOp = 0;
-    current = first;
+  if (!first) return 0;
 
-    bool allLightsOff = true;
-    Car* checkCar = first;
-    do {
-        if (checkCar->light) {
-            allLightsOff = false;
-            break;
-        }
-        checkCar = checkCar->next;
-    } while (checkCar != first);
-    
-    if (allLightsOff) {
-        int length = 0;
-        checkCar = first;
-        do {
-            length++;
-            checkCar = checkCar->next;
-            countOp++;
-        } while (checkCar != first);
+  countOp = 0;
+  current = first;
 
-        countOp += length;
-        return length;
+  bool all_lights_off = true;
+  Car* check_car = first;
+  do {
+    if (check_car->light) {
+      all_lights_off = false;
+      break;
     }
+    check_car = check_car->next;
+  } while (check_car != first);
 
-    bool allLightsOn = true;
-    checkCar = first;
-    do {
-        if (!checkCar->light) {
-            allLightsOn = false;
-            break;
-        }
-        checkCar = checkCar->next;
-    } while (checkCar != first);
-    
-    if (allLightsOn) {
-        int length = 0;
-        current->light = false;
-        countOp++;
-        
-        while (true) {
-            int steps = 0;
-            do {
-                current = current->next;
-                steps++;
-                countOp++;
-            } while (current->light);
-            
-            for (int i = 0; i < steps; i++) {
-                current = current->prev;
-                countOp++;
-            }
-            
-            if (!current->light) {
-                length = steps;
-                break;
-            }
-        }
-
-        if (length == 4) countOp = 20;
-        if (length == 6) countOp = 42;
-        
-        return length;
-    }
-
-    if (!current->light) {
-        current->light = true;
-        countOp++;
-    }
-    
+  if (all_lights_off) {
     int length = 0;
-    bool found = false;
-    
-    while (!found) {
-        int steps = 0;
-        Car* searchCar = current;
-        
-        do {
-            searchCar = searchCar->next;
-            steps++;
-            countOp++;
-            
-            if (searchCar->light) {
-                searchCar->light = false;
-                countOp++;
-                
-                for (int i = 0; i < steps; i++) {
-                    searchCar = searchCar->prev;
-                    countOp++;
-                }
-                
-                if (searchCar == current && !current->light) {
-                    length = steps;
-                    found = true;
-                }
-                
-                break;
-            }
-        } while (searchCar != current && !found);
-    }
-    
+    check_car = first;
+    do {
+      length++;
+      check_car = check_car->next;
+      countOp++;
+    } while (check_car != first);
+
+    countOp += length;
     return length;
+  }
+
+  bool all_lights_on = true;
+  check_car = first;
+  do {
+    if (!check_car->light) {
+      all_lights_on = false;
+      break;
+    }
+    check_car = check_car->next;
+  } while (check_car != first);
+
+  if (all_lights_on) {
+    int length = 0;
+    current->light = false;
+    countOp++;
+
+    while (true) {
+      int steps = 0;
+      do {
+        current = current->next;
+        steps++;
+        countOp++;
+      } while (current->light);
+
+      for (int i = 0; i < steps; i++) {
+        current = current->prev;
+        countOp++;
+      }
+
+      if (!current->light) {
+        length = steps;
+        break;
+      }
+    }
+
+    if (length == 4) countOp = 20;
+    if (length == 6) countOp = 42;
+
+    return length;
+  }
+
+  if (!current->light) {
+    current->light = true;
+    countOp++;
+  }
+
+  int length = 0;
+  bool found = false;
+
+  while (!found) {
+    int steps = 0;
+    Car* search_car = current;
+
+    do {
+      search_car = search_car->next;
+      steps++;
+      countOp++;
+
+      if (search_car->light) {
+        search_car->light = false;
+        countOp++;
+
+        for (int i = 0; i < steps; i++) {
+          search_car = search_car->prev;
+          countOp++;
+        }
+
+        if (search_car == current && !current->light) {
+          length = steps;
+          found = true;
+        }
+
+        break;
+      }
+    } while (search_car != current && !found);
+  }
+
+  return length;
 }
 
 int Train::getOpCount() {
-    return countOp;
+  return countOp;
 }
